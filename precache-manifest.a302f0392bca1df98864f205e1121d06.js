055372430b36088fc3c9649540d9729b6ff1a2ca
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-08-02 01:09:41 +0530

    Updates

---
+++ precache-manifest.a302f0392bca1df98864f205e1121d06.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9656aa8c8bc05ad6de943b6b66c7ca",
+    "url": "/breaking-bad-cards/index.html"
+  },
+  {
+    "revision": "73aa4cc40d5c859c9890",
+    "url": "/breaking-bad-cards/static/css/main.3ad5b5b2.chunk.css"
+  },
+  {
+    "revision": "7ebacee9e05e9a1aef92",
+    "url": "/breaking-bad-cards/static/js/2.42516c04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cards/static/js/2.42516c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73aa4cc40d5c859c9890",
+    "url": "/breaking-bad-cards/static/js/main.49c116a8.chunk.js"
+  },
+  {
+    "revision": "beff4eab55884f3a85f1",
+    "url": "/breaking-bad-cards/static/js/runtime-main.dd0c622e.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cards/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cards/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cards/static/media/spinner.235b9b3e.gif"
+  }
+]);
